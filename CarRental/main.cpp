2e5a87f2c33ce17fb6a4bb005b7fd522e5f689db
MantasKul <m.kulikauskas01@gmail.com>
2022-09-15 09:46:22 +0200

    Added empty project to start

---
+++ CarRental/main.cpp
@@ -0,0 +1,17 @@
+/*
+	Car list that has:
+	- carID
+	- License plat
+	- Make
+	- Model
+	- Color
+	- Availablility
+	
+	Renters list:
+	- rentID
+	- Full name (split in two?)
+	- Car's ID that is being rented (any other car info?)
+	- Perhaps other notes, like known dents/other issues (for company and renter insurance)
+
+
+*/
